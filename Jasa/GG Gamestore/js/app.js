421be9463581f13d79fe04039ca425836bd7a728
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2023-07-07 18:51:03 +0200

    1

--- Jasa/GG Gamestore/js/app.js
+++ Jasa/GG Gamestore/js/app.js
@@ -1,12 +1,12 @@
-(function($, document, window){
-	
-	$(document).ready(function(){
+(function ($, document, window) {
+
+	$(document).ready(function () {
 
 		// Cloning main navigation for mobile menu
 		$(".mobile-navigation").append($(".main-navigation .menu").clone());
 
 		// Mobile menu toggle 
-		$(".toggle-menu").click(function(){
+		$(".toggle-menu").click(function () {
 			$(".mobile-navigation").slideToggle();
 		});
 
@@ -15,36 +15,36 @@
 			directionNav: false
 		});
 
-		$(".login-button").on("click",function(){
+		$(".login-button").on("click", function () {
 			$(".overlay").fadeIn();
 			$(".auth-popup").toggleClass("active");
 		});
 
-		$(".close, .overlay").on("click",function(){
+		$(".close, .overlay").on("click", function () {
 			$(".overlay").fadeOut();
 			$(".popup").toggleClass("active");
 		});
 
 		initLightbox({
-	    	selector : '.product-images a',
-	    	overlay: true,
-	    	closeButton: true,
-	    	arrow: true
-	    });
+			selector: '.product-images a',
+			overlay: true,
+			closeButton: true,
+			arrow: true
+		});
 
 
-		$(document).keyup(function(e) {
-			if( $(".popup").hasClass("active")){
-		  		if (e.keyCode === 27) {
-		  			$(".overlay").fadeOut();
+		$(document).keyup(function (e) {
+			if ($(".popup").hasClass("active")) {
+				if (e.keyCode === 27) {
+					$(".overlay").fadeOut();
 					$(".popup").toggleClass("active");
-		  		}   
+				}
 			}
 		});
 	});
 
-	$(window).load(function(){
+	$(window).load(function () {
 
 	});
 
-})(jQuery, document, window);
+})
